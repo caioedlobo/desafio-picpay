1c14f35b4081d1b4f5ff694795e6d85fb3b1c7ba
caioedlobo <caioeduardolobo@gmail.com>
2024-03-21 19:11:16 -0300

    cria entity Usuario

--- src/main/java/com/caiolobo/desafiopicpay/Usuario.java
+++ src/main/java/com/caiolobo/desafiopicpay/Usuario.java
@@ -1,10 +1,80 @@
 package com.caiolobo.desafiopicpay;
 
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Table(name = "usuarios")
+@Entity
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
     private String nome;
     private String documento;
     private String email;
     private String senha;
+    private BigDecimal saldo;
 
+    public Usuario() {
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", documento='" + documento + '\'' +
+                ", email='" + email + '\'' +
+                ", senha='" + senha + '\'' +
+                ", saldo=" + saldo +
+                '}';
+    }
 }
