183aec4b57ec79329c76b3b1ff9971abf31a5f58
caioedlobo <caioeduardolobo@gmail.com>
2024-03-22 19:41:15 -0300

    adiciona busca se usuarioExiste

--- src/main/java/com/caiolobo/desafiopicpay/UsuarioRepository.java
+++ src/main/java/com/caiolobo/desafiopicpay/UsuarioRepository.java
@@ -11,6 +11,9 @@ import java.math.BigDecimal;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+    @Query("select count(u) > 0 from Usuario u where u.email = :email")
+    boolean existeUsuario(String email);
+
     @Transactional
     @Modifying
     @Query("update Usuario u set u.saldo = :novoSaldo where u.email = :email")
