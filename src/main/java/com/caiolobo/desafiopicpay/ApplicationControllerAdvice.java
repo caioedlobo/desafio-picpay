67e33c90b4cd65a28ca7fc232f44affc9b2eee1a
caioedlobo <caioeduardolobo@gmail.com>
2024-03-23 23:29:00 -0300

    adiciona handler de esception para Validações de usuário

--- src/main/java/com/caiolobo/desafiopicpay/ApplicationControllerAdvice.java
+++ src/main/java/com/caiolobo/desafiopicpay/ApplicationControllerAdvice.java
@@ -2,9 +2,12 @@ package com.caiolobo.desafiopicpay;
 
 import com.caiolobo.desafiopicpay.exceptions.UsuarioJaExisteException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
@@ -14,4 +17,14 @@ public class ApplicationControllerAdvice {
     public ApiErrors usuarioJaExisteException(UsuarioJaExisteException exception){
         return new ApiErrors(exception.getMessage());
     }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleMethodNotValidException(MethodArgumentNotValidException ex ){
+        List<String> errors = ex.getAllErrors()
+                .stream()
+                .map(error -> error.getDefaultMessage())
+                .collect(Collectors.toList());
+        return new ApiErrors(errors);
+    }
+
 }
