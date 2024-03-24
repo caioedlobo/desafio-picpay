d59a738c16ca66e03ecc73b45b227f56a61f5e16
caioedlobo <caioeduardolobo@gmail.com>
2024-03-23 23:29:27 -0300

    Adiciona validação no dto de criar usuário

--- src/main/java/com/caiolobo/desafiopicpay/CreateUsuarioDTO.java
+++ src/main/java/com/caiolobo/desafiopicpay/CreateUsuarioDTO.java
@@ -1,4 +1,12 @@
 package com.caiolobo.desafiopicpay;
 
-public record CreateUsuarioDTO(Long id, String nome, String documento, String email, String senha) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record CreateUsuarioDTO(Long id,
+                               @NotBlank String nome,
+                               @Pattern(regexp = "^\\d{11}|\\d{14}$", message = "CPF ou CNPJ inválido") String documento,
+                               @Email String email,
+                               @NotBlank String senha) {
 }
