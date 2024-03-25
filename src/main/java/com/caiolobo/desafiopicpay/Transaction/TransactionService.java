6e67c75ff208371f5e97c858653d02486ebe8a4e
caioedlobo <caioeduardolobo@gmail.com>
2024-03-25 20:16:03 -0300

    cria validação para transferência

--- src/main/java/com/caiolobo/desafiopicpay/Transaction/TransactionService.java
+++ src/main/java/com/caiolobo/desafiopicpay/Transaction/TransactionService.java
@@ -1,7 +1,11 @@
 package com.caiolobo.desafiopicpay.Transaction;
 
+import com.caiolobo.desafiopicpay.AccountType;
+import com.caiolobo.desafiopicpay.Usuario;
 import com.caiolobo.desafiopicpay.UsuarioService;
+import com.caiolobo.desafiopicpay.exceptions.ValidateException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class TransactionService {
@@ -13,8 +17,25 @@ public class TransactionService {
         this.usuarioService = usuarioService;
     }
 
+    @Transactional
     public void transfer(Transaction transaction){
-        usuarioService.withdraw(transaction.payer(), transaction.value());
-        usuarioService.deposit(transaction.payee(), transaction.value());
+        validate(transaction);
+        transactionRepository.save(transaction);
+        usuarioService.withdraw(transaction.getPayer(), transaction.getValue());
+        usuarioService.deposit(transaction.getPayee(), transaction.getValue());
+
+    }
+
+    private void validate(Transaction transaction){
+        Usuario payeer = usuarioService.searchAccount(transaction.getPayer());
+        usuarioService.searchAccount(transaction.getPayee());
+        if(payeer.getType() == AccountType.PJ.getValue()){
+            throw new ValidateException("Conta é PJ");
+        }
+        if(payeer.getSaldo().compareTo(transaction.getValue()) < 0){
+            throw new ValidateException("Saldo insuficiente para realizar transferência");
+        }
+
+
     }
 }
