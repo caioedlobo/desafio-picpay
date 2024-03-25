419fff69f59f209de319586b956a2843c8f8bd7b
caioedlobo <caioeduardolobo@gmail.com>
2024-03-25 19:31:54 -0300

    altera Transaction de record para class

--- src/main/java/com/caiolobo/desafiopicpay/Transaction/Transaction.java
+++ src/main/java/com/caiolobo/desafiopicpay/Transaction/Transaction.java
@@ -7,14 +7,64 @@ import org.springframework.data.annotation.CreatedDate;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-@Table(name= "transactions")
-public record Transaction(
-        @Id Long id,
-        Long payer,
-        Long payee,
-        BigDecimal value,
-        @CreatedDate LocalDateTime createdAt) {
-    public Transaction {
-        value = value.setScale(2);
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long payer;
+
+    private Long payee;
+
+    private BigDecimal value;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+
+    public Transaction(Long payer, Long payee, BigDecimal value) {
+        this.payer = payer;
+        this.payee = payee;
+        this.value = value.setScale(2);
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getPayer() {
+        return payer;
+    }
+
+    public void setPayer(Long payer) {
+        this.payer = payer;
+    }
+
+    public Long getPayee() {
+        return payee;
+    }
+
+    public void setPayee(Long payee) {
+        this.payee = payee;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
     }
 }
