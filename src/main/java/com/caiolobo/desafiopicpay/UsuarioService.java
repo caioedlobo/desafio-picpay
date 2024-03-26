35cbb9e8a2a08945b91f1c5c0a52295d0300bbb1
caioedlobo <caioeduardolobo@gmail.com>
2024-03-26 15:13:35 -0300

    adiciona metodo de deposit e withdraw no UsuarioService

--- src/main/java/com/caiolobo/desafiopicpay/UsuarioService.java
+++ src/main/java/com/caiolobo/desafiopicpay/UsuarioService.java
@@ -1,5 +1,6 @@
 package com.caiolobo.desafiopicpay;
 
+import com.caiolobo.desafiopicpay.exceptions.AccountNotFoundException;
 import com.caiolobo.desafiopicpay.exceptions.UsuarioJaExisteException;
 import org.springframework.stereotype.Service;
 
@@ -15,15 +16,23 @@ public class UsuarioService {
     }
 
     public Usuario criaUsuario(CreateUsuarioDTO novoUsuario){
-        if(usuarioRepository.existeUsuario(novoUsuario.email())){
+        if(usuarioRepository.userExists(novoUsuario.email())){
             throw new UsuarioJaExisteException();
         }
         return usuarioRepository.save(new Usuario(novoUsuario));
     }
 
-    /*public void enviarDinheiro(BigDecimal quantidade, String emailRecebedor){
-        if (transferenciaValida()){
-            usuarioRepository.atualizaDinheiro(quantidade, emailRecebedor);
+    public Usuario searchAccount(Long accountId) {
+        return usuarioRepository.findById(accountId).orElseThrow(AccountNotFoundException::new);
+    }
+
+    public void withdraw(Long user, BigDecimal value){
+        searchAccount(user);
+        usuarioRepository.withdraw(user, value);
+    }
+
+    public void deposit(Long user, BigDecimal value){
+        searchAccount(user);
+        usuarioRepository.deposit(user, value);
         }
-    }*/
 }
