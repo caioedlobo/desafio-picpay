222a695be5f53857d6c6c5f0c4b0b6768ca6c342
caioedlobo <caioeduardolobo@gmail.com>
2024-04-01 21:10:18 -0300

    create logger and calls authorize method on TransactionService

--- src/main/java/com/caiolobo/desafiopicpay/transaction/TransactionService.java
+++ src/main/java/com/caiolobo/desafiopicpay/transaction/TransactionService.java
@@ -3,18 +3,25 @@ package com.caiolobo.desafiopicpay.transaction;
 import com.caiolobo.desafiopicpay.AccountType;
 import com.caiolobo.desafiopicpay.Usuario;
 import com.caiolobo.desafiopicpay.UsuarioService;
+import com.caiolobo.desafiopicpay.authorization.AuthorizationService;
 import com.caiolobo.desafiopicpay.exceptions.ValidateException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Service
 public class TransactionService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TransactionService.class);
     private final TransactionRepository transactionRepository;
     private final UsuarioService usuarioService;
+    private final AuthorizationService authorizationService;
 
-    public TransactionService(TransactionRepository transactionRepository, UsuarioService usuarioService) {
+    public TransactionService(TransactionRepository transactionRepository, UsuarioService usuarioService, AuthorizationService authorizationService) {
         this.transactionRepository = transactionRepository;
         this.usuarioService = usuarioService;
+        this.authorizationService = authorizationService;
     }
 
     @Transactional
@@ -24,9 +31,12 @@ public class TransactionService {
         usuarioService.withdraw(transaction.getPayer(), transaction.getValue());
         usuarioService.deposit(transaction.getPayee(), transaction.getValue());
 
+        authorizationService.authorize(transaction);
+
     }
 
     private void validate(Transaction transaction){
+        LOGGER.info("Validating transaction {}", transaction);
         Usuario payeer = usuarioService.searchAccount(transaction.getPayer());
         usuarioService.searchAccount(transaction.getPayee());
         if(payeer.getType() == AccountType.PJ.getValue()){
