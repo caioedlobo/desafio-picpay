00c3a711d444d7279c36ded6337e23dbf585c789
caioedlobo <caioeduardolobo@gmail.com>
2024-05-04 14:52:54 -0300

    refac Authroization record to use only isAuthorized method

--- src/main/java/com/caiolobo/desafiopicpay/authorization/Authorization.java
+++ src/main/java/com/caiolobo/desafiopicpay/authorization/Authorization.java
@@ -1,12 +1,9 @@
 package com.caiolobo.desafiopicpay.authorization;
 
-public record Authorization(Long id, ResponseAuthorization response) {
+public record Authorization(String message) {
 
 
-    public boolean isNotificationAuthorized(){
-        return response().notification();
-    }
-    public boolean isSmsAuthorized(){
-        return response().sms();
+    boolean isAuthorized(){
+        return message().equalsIgnoreCase("Autorizado");
     }
 }
